c26bc67b1b7337e0dabdc207516f49ef6cfb2002
Kamila Mattos <99353050+KamilaMattos@users.noreply.github.com>
2022-12-03 15:55:38 -0300

    config: config server and app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,24 +1,17 @@
-import express, { Request, Response } from "express"
-import cors from "cors"
-import { handleErrorMiddleware } from "./middlewares/errors.middleware"
-import "reflect-metadata"
 import "express-async-errors"
+import "reflect-metadata"
+
+import express from "express"
+import cors from "cors"
+
+import { handleErrorMiddleware } from "./middlewares/errors.middleware"
 import { appRoutes } from "./routes"
 
 const app = express()
-
 app.use(express.json())
-
 app.use(cors())
 
 appRoutes(app)
-
-app.get("/", (req: Request, res: Response) => {
-  res.status(200).json({
-    message: "Hello!",
-  })
-})
-
 app.use(handleErrorMiddleware)
 
 export default app
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,11 @@
-import app from "./app";
-import AppDataSource from "./data-source";
+import app from "./app"
+import AppDataSource from "./data-source"
 
-(async () => {
-
-    await AppDataSource.initialize()
-    .catch((err) => {
-        console.error("Error during Data Source initialization", err)
-    })
-    
-    app.listen(3000, () => {
-        console.log("Servidor executando")
-    })    
-})()
+const init = async () => {
+  const PORT = process.env.PORT || 3001
+  await AppDataSource.initialize()
+  app.listen(PORT, () => {
+    console.log(`App is running in port ${PORT}!`)
+  })
+}
+init()
