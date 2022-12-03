7d0982c60b3b65b3f3744b4f6a889237bae99d4c
Kamila Mattos <99353050+KamilaMattos@users.noreply.github.com>
2022-12-03 15:27:10 -0300

    feat: user routes done

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,13 +1,18 @@
 import { Router } from "express"
+import { createUserController } from "../controllers/users/createUser.controller"
+import { deleteUserController } from "../controllers/users/deleteUser.controller"
+import { listOneUserController } from "../controllers/users/listOneUser.controller"
+import { listUsersController } from "../controllers/users/listUsers.controller"
+import { updateUserController } from "../controllers/users/updateUser.controller"
 
 const routes = Router()
 
 export const userRoutes = () => {
-  routes.post("")
-  routes.get("")
-  routes.get("/:id")
-  routes.delete("/:id")
-  routes.patch("/:id")
+  routes.post("", createUserController)
+  routes.get("", listUsersController)
+  routes.get("/:id", listOneUserController)
+  routes.patch("/:id", updateUserController)
+  routes.delete("/:id", deleteUserController)
 
   return routes
 }
